12a8a1b9601d0896f3d4def3a1c011937911d422
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-24 00:13:46 -0400

    feat: add notification icon

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,20 +2,29 @@
 import { useNotification } from '../../contexts/NotificationContext';
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import './Notification.scss'
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { MdErrorOutline } from 'react-icons/md';
+import { CiCircleInfo } from 'react-icons/ci';
+import type { JSX } from 'react';
 
 function Notification() {
     const { notification, clearNotification } = useNotification();
 
     if (!notification) return null;
 
+    const iconMap: Record<typeof notification.type, JSX.Element> = {
+        success: <AiOutlineCheckCircle />,
+        error: <MdErrorOutline />,
+        info: <CiCircleInfo />,
+    };
+
     return (
         <div className='notification' style={{
             backgroundColor: notification.color,
         }}>
-            {notification.message}
-            
+            <span className="icon">{iconMap[notification.type]}</span>
+            <span className="message">{notification.message}</span>
             <IoMdCloseCircleOutline onClick={clearNotification} className='btn'/>
-            
         </div>
     )
 }
